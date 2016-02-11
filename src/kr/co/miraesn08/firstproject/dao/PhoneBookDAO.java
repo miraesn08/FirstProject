4fe6ed91469e62b4cf6344133c534be356047867
miraesn08 <miraesn08@gmail.com>
2016-02-11 09:32:40 +0900

    add PhoneBookDAO(interface)

---
+++ src/kr/co/miraesn08/firstproject/dao/PhoneBookDAO.java
@@ -0,0 +1,18 @@
+package kr.co.miraesn08.firstproject.dao;
+
+import java.util.List;
+
+import kr.co.miraesn08.firstproject.dto.PhoneBookDTO;
+
+public interface PhoneBookDAO {
+	public boolean add(PhoneBookDTO phoneBook);
+	public boolean update(PhoneBookDTO phoneBook);
+	public boolean delete(int id);
+	public PhoneBookDTO get(int id);
+	public List<PhoneBookDTO> getList();
+	public List<PhoneBookDTO> getListByField(String fieldName, String searchValue);
+	public List<PhoneBookDTO> getListByName(String searchName);
+	
+	public boolean create();
+	public boolean drop();
+}
