64267ebc8460212eeae4206a73fbb317cca40e11
miraesn08 <miraesn08@gmail.com>
2016-02-11 20:22:15 +0900

    add DBError

---
+++ src/kr/co/miraesn08/firstproject/common/DBError.java
@@ -0,0 +1,8 @@
+package kr.co.miraesn08.firstproject.common;
+
+public class DBError {
+	public static int ERROR_CODE_OK = 0;
+	public static int ERROR_CODE_NO_DATA = -1;
+	public static int ERROR_CODE_INVALID_PARAMETER = -2;
+	public static int ERROR_CODE_DAO_ERROR = -3;
+}
